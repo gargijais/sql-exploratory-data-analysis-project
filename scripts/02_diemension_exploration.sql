7a6445653945108698423233373e71d0a9cba15d
gargijais <jaiswalgargi.2000@gmail.com>
2026-09-13 16:26:30 +0530

    Create 02_diemension_exploration.sql

---
+++ scripts/02_diemension_exploration.sql
@@ -0,0 +1,23 @@
+/* ===========================================================================
+   Diemention Exploration
+   ===========================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+=========================================================================== */
+-- -- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT
+	country
+FROM gold.dim_customers
+ORDER BY country;
+
+-- -- Retrieve a list of unique categories, subcategories - "The Major Divisions'
+SELECT DISTINCT
+	category,
+	subcategory,
+	product_name
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
